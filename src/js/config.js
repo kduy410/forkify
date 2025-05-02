dcb3a9d871cf539fe5600f0105a73ec64b6548fd
Duy <kduy410@gmail.com>
2025-05-02 18:39:14 +0700

    update

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR KEY>';
+export const KEY = '023b296f-3fab-4f29-bc45-b446c22ecbf1';
 export const MODAL_CLOSE_SEC = 2.5;
